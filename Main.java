46e8b48277b298ea873d3a53eec28b7723073c6b
Alejosss <aggomez@scl.edu.gt>
2025-05-19 03:07:56 -0600

    Version1

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 // Importa la clase JFrame del paquete javax.swing (para crear la ventana)
-import javax.swing.JFrame;
-// Importa la clase JLabel del paquete javax.swing (para crear etiquetas de texto)
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import vistas.LoginView;
+
 
 // Clase principal que contiene el método main
 public class Main {
 
     // Método principal, punto de entrada de la aplicación
     public static void main(String[] args) {
+
+
+        //Ventana
         
         // Crea una instancia de JFrame (ventana gráfica)
         JFrame miVentana = new JFrame();
@@ -15,19 +24,34 @@ public class Main {
         // Establece el título que aparecerá en la barra superior de la ventana
         miVentana.setTitle("Hospital SCL");
         
-        // Define el tamaño inicial de la ventana (ancho: 1000px, alto: 800px)
+       
         miVentana.setSize(1000,800);
+        miVentana.setLayout(new BorderLayout());
+
+
         
-        // Crea una instancia de JLabel (etiqueta para mostrar texto)
-        JLabel texto = new JLabel();
+
+        JPanel panelBanner= new JPanel();
+        panelBanner.setPreferredSize(new Dimension(500,400));
+        panelBanner.setOpaque(true);
+        panelBanner.setBackground(Color.RED);
+    
+        miVentana.add(panelBanner,BorderLayout.WEST);
+      
+
+
+
+  
+
+        LoginView panelLogin =new LoginView();
+
+        miVentana.add(panelLogin.getJPanel());
+
+        miVentana.add(panelLogin.getJPanel(), BorderLayout.CENTER);
+
+
         
-        // Establece el texto que mostrará la etiqueta
-        texto.setText("Hola Mundo:D");
-        
-        // Añade la etiqueta (JLabel) a la ventana (JFrame)
-        miVentana.add(texto);
-        
-        // Hace visible la ventana (sin esta línea la ventana no se mostraría)
+    
         miVentana.setVisible(true);
     }
 }
---
+++ vistas/LoginView.java
@@ -0,0 +1,76 @@
+package vistas;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.*;
+
+public class LoginView {
+
+    private JPanel panelLogin;
+
+    public LoginView(){
+
+        panelLogin = new JPanel();
+        panelLogin.setSize(500,800);
+        panelLogin.setOpaque(true);
+        panelLogin.setBackground(Color.WHITE);
+        panelLogin.setLayout(new GridBagLayout());
+
+
+        //Administrador de diseño
+      GridBagConstraints gbc=new GridBagConstraints();
+        gbc.insets= new Insets(10, 80, 10, 20);
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.weightx=1.0;
+
+
+// Título
+        JLabel titulo = new JLabel("Login", SwingConstants.CENTER);
+       
+    
+       gbc.anchor=GridBagConstraints.CENTER;
+        gbc.gridx=0;
+        gbc.gridy=0;
+        gbc.gridwidth=2;
+       
+        
+        panelLogin.add(titulo,gbc);
+      
+
+// Entrada de correo
+
+        JTextField entradaCorreo = new JTextField("Usuario: ");
+        gbc.gridx=1;
+        gbc.gridy=1;
+        panelLogin.add(entradaCorreo,gbc);
+       
+
+// Entrada contraseña
+
+        JPasswordField entradaContra = new JPasswordField("Contraseña: ");
+        entradaContra.setSize(new Dimension(100,50));
+        gbc.gridx=1     ;
+        gbc.gridy=2;
+        
+        panelLogin.add(entradaContra,gbc);
+// botón
+
+        JButton entradaBoton = new JButton("Iniciar");
+        entradaBoton.setPreferredSize(new Dimension(100,30));
+        gbc.gridx=1;
+        gbc.gridy=3;
+        gbc.anchor=GridBagConstraints.EAST;
+        gbc.fill=GridBagConstraints.NONE;
+        
+        
+        panelLogin.add(entradaBoton,gbc);
+    }
+    
+    public JPanel getJPanel(){
+        return panelLogin;
+    }
+}
