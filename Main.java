f574acf945bd03bf2b8420ac1d6412363ac76626
Alejosss <aggomez@scl.edu.gt>
2025-05-26 10:27:28 -0600

    Funcion del botón ingresar

--- Main.java
+++ Main.java
@@ -4,33 +4,23 @@ import javax.swing.*;
 import vistas.BannerPanel;
 import vistas.LoginView;
 
-
-
 public class Main {
 
-
     public static void main(String[] args) {
-  //Titulo y tamali de la ventana
+        // Título y tamaño de la ventana
         JFrame miVentana = new JFrame();
         miVentana.setTitle("Hospital SCL");
-        miVentana.setSize(1000,800);
+        miVentana.setSize(1000, 800);
         miVentana.setLayout(new BorderLayout());
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        // BannerPanel
+        BannerPanel panelBanner = new BannerPanel();
+        miVentana.add(panelBanner.getJPanel(), BorderLayout.WEST);
 
-
-        
-//BannerPannel
-      BannerPanel panelBanner = new BannerPanel();
-       miVentana.add(panelBanner.getJPanel(), BorderLayout.WEST);
-
-
-  
-//LoginView
-        LoginView panelLogin =new LoginView();
+        // LoginView (con frame pasado)
+        LoginView panelLogin = new LoginView(miVentana); 
         miVentana.add(panelLogin.getJPanel(), BorderLayout.CENTER);
 
-
-        
-    
         miVentana.setVisible(true);
     }
-}        
+}
--- vistas/LoginView.java
+++ vistas/LoginView.java
@@ -5,14 +5,16 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
+import java.awt.event.*;
 import javax.swing.*;
+import ventanas.*;
 
 public class LoginView {
 
     private JPanel panelLogin;
-
-    public LoginView(){
+    private JFrame frame; 
+    public LoginView(JFrame frame){ 
+        this.frame = frame; 
 
         panelLogin = new JPanel();
         panelLogin.setSize(500,800);
@@ -20,56 +22,51 @@ public class LoginView {
         panelLogin.setBackground(Color.WHITE);
         panelLogin.setLayout(new GridBagLayout());
 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 80, 10, 20);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
 
-        //Administrador de diseño
-      GridBagConstraints gbc=new GridBagConstraints();
-        gbc.insets= new Insets(10, 80, 10, 20);
-        gbc.fill=GridBagConstraints.HORIZONTAL;
-        gbc.weightx=1.0;
-
-
-// Título
+        // Título
         JLabel titulo = new JLabel("Login", SwingConstants.CENTER);
-       
-    
-       gbc.anchor=GridBagConstraints.CENTER;
-        gbc.gridx=0;
-        gbc.gridy=0;
-        gbc.gridwidth=2;
-       
-        
-        panelLogin.add(titulo,gbc);
-      
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        panelLogin.add(titulo, gbc);
 
-// Entrada de correo
-
+        // Entrada de correo
         JTextField entradaCorreo = new JTextField("Usuario: ");
-        gbc.gridx=1;
-        gbc.gridy=1;
-        panelLogin.add(entradaCorreo,gbc);
-       
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panelLogin.add(entradaCorreo, gbc);
 
-// Entrada contraseña
-
+        // Entrada contraseña
         JPasswordField entradaContra = new JPasswordField("Contraseña: ");
-        entradaContra.setSize(new Dimension(100,50));
-        gbc.gridx=1     ;
-        gbc.gridy=2;
-        
-        panelLogin.add(entradaContra,gbc);
-// botón
+        entradaContra.setSize(new Dimension(100, 50));
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        panelLogin.add(entradaContra, gbc);
 
+        // Botón
         JButton entradaBoton = new JButton("Iniciar");
-        entradaBoton.setPreferredSize(new Dimension(100,30));
-        gbc.gridx=1;
-        gbc.gridy=3;
-        gbc.anchor=GridBagConstraints.EAST;
-        gbc.fill=GridBagConstraints.NONE;
-        
-        
-        panelLogin.add(entradaBoton,gbc);
+        entradaBoton.setPreferredSize(new Dimension(100, 30));
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        panelLogin.add(entradaBoton, gbc);
+
+        // Acción del botón
+        entradaBoton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                frame.dispose(); 
+                new DashBoard(); 
+            }
+        });
     }
-    
+
     public JPanel getJPanel(){
         return panelLogin;
     }
